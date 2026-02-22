900632c4c95ccf4ba6efc836c255c7737910febd
Narumikr <narunarumikumiku396@gmail.com>
2026-02-22 20:10:36 +0900

    edit workflow

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const _count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="_count.value++">count is {{ _count.value }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
